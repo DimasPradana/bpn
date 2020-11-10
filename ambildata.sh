861e413d7cc4a14185b9bfd5281eae05b9fc2796
Dimas Pradana <deemaazbaek@gmail.com>
2020-11-10 14:49:49 +0700

    tambah parameter di ambildata.sh

--- ambildata.sh
+++ ambildata.sh
@@ -3,6 +3,10 @@
 for number in {1..31}
 do
   # reset && go run main.go -tgl="$number/05/2020"
-  go run main.go -tgl="$number/$@/2020"
+  # go run main.go -tgl="$number/$@/2020"
+  go run main.go -tgl="$number/$1/$2"
+  # echo "number : $number"
+  # echo "masukkan bulan dengan format angka 2 digit: $1"
+  # echo "masukkan tahun dengan format angka 4 digit: $2"
 done
 exit 0
